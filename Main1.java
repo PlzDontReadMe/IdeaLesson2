1e18403d8d8c1f944343deb0b9ab09403f903523
Aqz <Kzak.Gluhov@gmail.com>
2022-03-31 11:16:01 +0300

    main1

---
+++ Main1.java
@@ -0,0 +1,9 @@
+public class Main1 {
+    public static void main(String[] args) {
+        int ticketPrice = 200;
+        int milesPrice = 20;
+
+        int bonusMiles = ticketPrice / milesPrice;
+        System.out.println(bonusMiles);
+    }
+}
